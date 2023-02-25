27ea26d57b46605663d033889aa86b36cbcbdb2c
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-24 18:51:23 -0600

    update data

--- src/store/map/actions.ts
+++ src/store/map/actions.ts
@@ -9,7 +9,10 @@ export type LngLat = [number, number]
 const actions: ActionTree<MapState, StateInterface> = {
     async getRouteBetweenPoints({ commit }, { start, end }: { start: LngLat, end: LngLat }) {
         const resp = await directionsApi.get<DirectionsResponse>(`${start.join(',')};${end.join(',')}`)
-        console.log(resp.data.routes[0].geometry.coordinates);
+
+        //TODO: dISTANCIA Y DURACION
+
+        commit('setRoutePolyline', resp.data.routes[0].geometry.coordinates)
     }
 }
 
--- src/store/map/mutations.ts
+++ src/store/map/mutations.ts
@@ -1,8 +1,10 @@
 import { MutationTree } from 'vuex';
 import { MapState } from './state';
 import Mapboxgl from 'mapbox-gl';
-import { Feature } from '../../interfaces/places';
+import { Feature, Properties } from '../../interfaces/places';
 
+/** Route String Name */
+const RouteString = 'RouteString'
 
 const mutation: MutationTree<MapState> = {
     setMap(state, map: Mapboxgl.Map) {
@@ -33,6 +35,75 @@ const mutation: MutationTree<MapState> = {
 
             state.markers.push(marker)
         }
+
+        // Clear del polyline
+        if (state.map.getLayer(RouteString)) {
+            state.map.removeLayer(RouteString)
+            state.map.removeSource(RouteString)
+            state.distance = undefined
+            state.duration = undefined
+        }
+    },
+    setRoutePolyline(state, coords: number[][]) {
+        const start = coords[0]
+        const end = coords[coords.length - 1]
+
+        //definir los bounds => puntos contenidos en el mapa
+        const bounds = new Mapboxgl.LngLatBounds(
+            [start[0], start[1]],
+            [start[0], start[1]]
+        )
+
+        for (const coord of coords) {
+            const newCoord: [number, number] = [coord[0], coord[1]]
+            bounds.extend(newCoord)
+        }
+
+        state.map?.fitBounds(bounds, {
+            padding: 200
+        })
+
+        //Polyline
+        const sourceData: Mapboxgl.AnySourceData = {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: [
+                    {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                            type: 'LineString',
+                            coordinates: coords
+                        }
+                    }
+                ]
+            }
+        };
+
+        //Graficar
+        if (state.map?.getLayer(RouteString)) {
+            //remuevo la linea
+            state.map.removeLayer(RouteString)
+            //remuevo source
+            state.map.removeSource(RouteString)
+        }
+
+        state.map?.addSource(RouteString, sourceData)
+
+        state.map?.addLayer({
+            id: RouteString,
+            type: 'line',
+            source: RouteString,
+            layout: {
+                'line-cap': 'round',
+                'line-join': 'round'
+            },
+            paint: {
+                'line-color': 'black',
+                'line-width': 3
+            }
+        })
     }
 }
 
